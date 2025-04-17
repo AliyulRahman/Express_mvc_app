775587ff248ae2c51e7e4ee992c62203944b3aed
AliyulRahman <65935555+AliyulRahman@users.noreply.github.com>
2025-04-17 17:21:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,4 +27,8 @@ app.use(logger);
 // Routes
 app.use('/api/users', userRoutes);
 
-module.exports = app;
+// Start the server here
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
