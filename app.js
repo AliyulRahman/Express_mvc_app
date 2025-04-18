746da63722188426424f3d9dd91b0c025f7d263e
Mohamad Aliyul Rahman <aliyulrahman@gmail.com>
2025-04-18 10:35:03 +0300

    logger removed from middleware

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const path = require('path');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
-const logger = require('./libs/Middlewarefunctions/logger');
 
 dotenv.config();
 
@@ -22,7 +21,6 @@ const corsOptions = {
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(logger);
 
 // Routes
 app.use('/api/users', userRoutes);
